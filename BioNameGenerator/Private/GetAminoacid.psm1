a66e33b08884aa928875bc66933eaa9f11ab0c48
Davi Marcon (@mxrcon) <davijosuemarcon@gmail.com>
2022-06-21 18:07:13 -0300

    The query will ask for random

--- BioNameGenerator/Private/GetAminoacid.psm1
+++ BioNameGenerator/Private/GetAminoacid.psm1
@@ -1,8 +1,8 @@
 function Get-Aminoacid {
     param([parameter(Mandatory)][String]$DatabasePath)
 
-    $Table = Invoke-SqliteQuery -Database $DatabasePath -Query "SELECT * FROM Aminoacids"
-    $Aminoacids = $Table.Aminoacids
-    return $Aminoacids[$(Get-Random -Maximum $Aminoacids.count)]
+    $Table = Invoke-SqliteQuery -Database $DatabasePath -Query "SELECT * FROM Aminoacids ORDER BY RANDOM() LIMIT 1"
+    $Aminoacid = $Table.Aminoacids
+    return $Aminoacid
 }
 
