a23fee09b8bf39662a9b9112848823ba36e6584a
Davi Marcon (@mxrcon) <davijosuemarcon@gmail.com>
2022-06-21 20:35:09 -0300

    Typo on `GetBacterialSpecie`

--- BioNameGenerator/Private/GetBacterialSpecie.psm1
+++ BioNameGenerator/Private/GetBacterialSpecie.psm1
@@ -2,6 +2,6 @@ function Get-BacterialSpecie{
     param([parameter(Mandatory)][String]$DatabasePath)
 
     $Table = Invoke-SqliteQuery -Database $DatabasePath -Query "SELECT BacterialSpecies FROM BacterialSpecies ORDER BY RANDOM() LIMIT 1"
-    $BacterialSpecie = $Table.BacterialSpecie
+    $BacterialSpecie = $Table.BacterialSpecies
     return $BacterialSpecie
 }
