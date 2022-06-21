2fb3a786512aadce2ae86c173e2b69e4dcb7e769
Davi Marcon (@mxrcon) <davijosuemarcon@gmail.com>
2022-06-21 20:58:17 -0300

    Adding `Get-RandomKeyword`
    Generalized is always better

---
+++ BioNameGenerator/Private/GetRandomKeyword.psm1
@@ -0,0 +1,12 @@
+function Get-RandomKeyword {
+    param([parameter(Mandatory)][String]$DatabasePath,
+    [parameter(Mandatory)][String]$TableName,
+    [parameter(Mandatory)][String]$TableField)
+    
+
+    $SQLiteQuery = "Select $TableField From $TableName ORDER BY RANDOM() LIMIT 1"
+    $Table = Invoke-SqliteQuery -Database $DatabasePath -Query $SQLiteQuery
+
+    return $Table.$TableField
+}
+
