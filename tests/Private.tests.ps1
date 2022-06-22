8c16b1c3dfb2ee723643a0bec829d11f998267f0
Davi Marcon (@mxrcon) <davijosuemarcon@gmail.com>
2022-06-21 21:12:46 -0300

    updating Dictionaries Headers

--- tests/Private.tests.ps1
+++ tests/Private.tests.ps1
@@ -12,23 +12,23 @@ BeforeAll {
 
 Describe 'Test Private Functions' {
     It 'Generate a random Aminoacid' {
-	Get-RandomKeyword $ProjectRoot/$DatabasePath -TableField Aminoacids -TableName Aminoacids 
+	Get-RandomKeyword $ProjectRoot/$DatabasePath -TableField AminoacidName -TableName Aminoacids 
     }
 
     It 'Generate a random Nucleic Acid' {
-        Get-RandomKeyword -Database $ProjectRoot/$DatabasePath -TableField NucleicAcids -TableName NucleicAcids
+        Get-RandomKeyword -Database $ProjectRoot/$DatabasePath -TableField NucleicAcidName -TableName NucleicAcids
     }
 
     It 'Generate a random Bacterial Genera'{
-	Get-RandomKeyword -Database $ProjectRoot/$DatabasePath -TableField BacterialGeneras -TableName BacterialGeneras
+	Get-RandomKeyword -Database $ProjectRoot/$DatabasePath -TableField GeneraName -TableName BacterialGeneras
     }
 
     It 'Generate a random Bacterial Specie'{
-	Get-RandomKeyword -Database $ProjectRoot/$DatabasePath -TableField BacterialSpecies -TableName BacterialSpecies
+	Get-RandomKeyword -Database $ProjectRoot/$DatabasePath -TableField SpecieName -TableName BacterialSpecies
     }
 
     It 'Generate a random Metal or Alloy'{
-	Get-RandomKeyword -Database $ProjectRoot/$DatabasePath -TableField MetalsAndAlloys -TableName MetalsOrAlloys
+	Get-RandomKeyword -Database $ProjectRoot/$DatabasePath -TableField ElementName -TableName MetalsAndAlloys
     }
 
 }
